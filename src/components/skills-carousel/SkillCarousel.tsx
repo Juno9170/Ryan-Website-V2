439238337d07c0192acc6c43aa3ba5479b80bb2a
Juno9170 <ryanzhuturtle@gmail.com>
2024-09-16 19:52:46 -0400

    Icons Added

--- src/components/skills-carousel/SkillCarousel.tsx
+++ src/components/skills-carousel/SkillCarousel.tsx
@@ -6,7 +6,21 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import { ChevronDown, ChevronUp } from "lucide-react";
-const SkillCarousel = () => {
+interface PropsSchema {
+  skills: Array<SkillSchema>;
+}
+interface SkillSchema {
+  name: string;
+  relatedSkills: String[];
+  value: Record<string, any>;
+  icon: {
+    avifUrl?: string;
+    webpUrl?: string;
+    fallbackUrl: string;
+  };
+}
+
+const SkillCarousel: React.FC<PropsSchema> = ({ skills }) => {
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState<number>(0);
   const [dragging, setDragging] = useState<boolean>(false);
@@ -29,7 +43,6 @@ const SkillCarousel = () => {
     });
   }, [api]);
 
-  const skillsArray = Array.from({ length: 8 });
   return (
     <div className="flex">
       <div className="flex flex-col justify-center pr-5 ">
@@ -70,12 +83,25 @@ const SkillCarousel = () => {
             className="-mt-1 h-[90vh]
         "
           >
-            {skillsArray.map((_, index) => (
+            {skills.map((skill, index) => (
               <CarouselItem key={index} className="basis-1/5 pt-0 ">
                 <div
-                  className={`w-full h-full flex transition-all duration-100 ${dragging ? "border-t" : ""} ${index === (current + 2) % skillsArray.length && !dragging ? "border-t" : ""} ${index === (current - 2 < 0 ? current - 2 + skillsArray.length : current - 2) && !dragging ? "border-b" : ""} ${!dragging && index === current ? " border-y" : ""} justify-center items-center`}
+                  className={`w-full h-full flex items-center transition-all duration-100 ${dragging ? "border-t" : ""} ${index === (current + 2) % skills.length && !dragging ? "border-t" : ""} ${index === (current - 2 < 0 ? current - 2 + skills.length : current - 2) && !dragging ? "border-b" : ""} ${!dragging && index === current ? " border-y" : ""} `}
                 >
-                  {index}
+                  <div className=" mx-auto">
+                    <picture>
+                      <source srcSet={skill.icon.avifUrl} type="image/avif" />
+                      <source srcSet={skill.icon.webpUrl} type="image/webp" />
+                      <img
+                        src={skill.icon.fallbackUrl}
+                        alt={`${skill.name || ""} Skill Icon`}
+                        loading="eager"
+                        decoding="async"
+                        className=" xxl:h-28"
+                      />
+                    </picture>
+                  </div>
+                  <div className="">show</div>
                 </div>
               </CarouselItem>
             ))}
