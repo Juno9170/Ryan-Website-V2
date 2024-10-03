efd89b457df4254921df7c1a1ef4b88dad130e81
Juno9170 <ryanzhuturtle@gmail.com>
2024-10-03 12:55:28 -0400

    Screen Size responsive

--- src/components/contact-me/ContactForm.tsx
+++ src/components/contact-me/ContactForm.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Send } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,21 +13,21 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "../ui/textarea"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "../ui/textarea";
 
 const FormSchema = z.object({
   fullName: z.string().min(2, {
-    message: "name must be at least 2 characters.",
+    message: "Name must be at least 2 characters.",
   }),
   emailAddress: z.string().email({
-    message: "email not valid. Please enter a valid email.",
+    message: "Email not valid. Please enter a valid email.",
   }),
-  message: z.string().max(300, {
-    message: "message is too long! (300 character max).",
+  message: z.string().max(350, {
+    message: "Message is too long! (350 character max).",
   }),
-})
+});
 
 const ContactForm = () => {
   const form = useForm<z.infer<typeof FormSchema>>({
@@ -37,71 +37,108 @@ const ContactForm = () => {
       emailAddress: "",
       message: "",
     },
-  })
+  });
+
+  const { handleSubmit, control, formState } = form;
+  const { errors, isSubmitting } = formState;
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(data);
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-1/2 space-y-6 shadow-dark-short border-2 border-[#f3fefb] bg-[#dee7e4] rounded-[2.5rem] px-14 py-10">
-        <FormField
-          control={form.control}
-          name="fullName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Your Name</FormLabel>
-              <FormControl>
-                <Input placeholder="who's calling?" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is who I will address you as.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="emailAddress"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Your Email</FormLabel>
-              <FormControl>
-                <Input placeholder="example987@gmail.com?" {...field} />
-              </FormControl>
-              <FormDescription>
-                Please enter your full email address.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="message"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Message to me!</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="Tell us a little bit about yourself"
-                  className="resize-none"
-                  {...field} />
-              </FormControl>
-              <FormDescription>
-                Please enter your message here.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex justify-center pt-2">
-        <Button type="submit" className="h-full px-8 shadow-button-short rounded-full bg-[#8DB9AA] hover:bg-[#587d71]">Submit</Button>
-        </div>
-      </form>
-    </Form>
-  )
-}
+    <div className=" w-full lg:w-1/2 space-y-6 bg-gradient-to-tr from-transparent to-transparent via-white">
+      <Form {...form}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full space-y-6 shadow-dark-short border-2 border-[#f3fefb] bg-[#a1c2af70] rounded-[2.5rem] px-14 py-10"
+        >
+          <FormField
+            control={control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="xxl:text-xl xl:text-lg text-base">
+                  Your Name
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="who's calling?"
+                    {...field}
+                    className="xxl:text-xl xl:text-lg text-base"
+                  />
+                </FormControl>
+                <FormDescription>
+                  This is who I will address you as.
+                </FormDescription>
+                <FormMessage>
+                  {errors.fullName && <span>{errors.fullName.message}</span>}
+                </FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={control}
+            name="emailAddress"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="xxl:text-xl xl:text-lg text-base">
+                  Your Email
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="example987@gmail.com?"
+                    {...field}
+                    className="xxl:text-xl xl:text-lg text-base "
+                  />
+                </FormControl>
+                <FormDescription>
+                  Please enter your full email address.
+                </FormDescription>
+                <FormMessage>
+                  {errors.emailAddress && (
+                    <span>{errors.emailAddress.message}</span>
+                  )}
+                </FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="xxl:text-xl xl:text-lg text-base">
+                  Message to me!
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us a little bit about yourself"
+                    className="resize-none xxl:text-xl xl:text-lg text-base xxl:h-60"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Please enter your message here.
+                </FormDescription>
+                <FormMessage>
+                  {errors.message && <span>{errors.message.message}</span>}
+                </FormMessage>
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-center py-2">
+            <Button
+              type="submit"
+              className={`h-full px-8 shadow-button-short rounded-full bg-[#8DB9AA] hover:bg-[#587d71] ${isSubmitting || Object.keys(errors).length > 0 ? " cursor-not-allowed" : " cursor-pointer"}`}
+            >
+              {isSubmitting ? "Sending!..." : "Submit"}
+              <Send strokeWidth={2} className=" xxxl:h-6 xl:h-4 lg:h-3" />
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
 export default ContactForm;
